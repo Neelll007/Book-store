6e779459da619a95aa5c7e0823107cd4d52b3c6c
Neelll007 <neelchangawala@gmail.com>
2023-02-23 23:09:58 +0530

    Day 3 Task

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import Title from '../component/Title'
 class Home extends Component {
+    constructor(){
+        super()
+        this.state = {
+            title:"Book-store",
+            description:"Book-store is online book buying and selling web app.its give you all the latest book which is available in market.our web app also provide the all the old book also which is rare to find."
+        }
+    }
     render() {
         return (
             <div className="wp">
-                <Title text="Book-e-sell"/>
+                {/* <Title text="Book-e-sell"/>
                 <Title text="this is my react training project."/>
                 <Title text="this ia about component"/>
                 <Title text="Home"/>
                 <Title text="About"/>
-                <Title text="Login"/>
-
+                <Title text="Login"/> */}
+                <Title title={this.state.title} desc={this.state.description}/>
                 <div className="container">
 
-                    <h1>This is classbased Home Component</h1>
+                    <h3>classbased Home Component</h3>
                 </div>
             </div>
         );
